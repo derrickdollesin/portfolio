1a2192f0262e030220852a067000766a0d6a48cb
derrickdollesin <derdollesin@comcast.net>
2025-10-22 18:55:11 -0700

    part 1

--- global.js
+++ global.js
@@ -109,3 +109,40 @@ form?.addEventListener('submit', function (event) {
 
   location.href = url;
 });
+
+/* Projects */
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  // Your code will go here
+  
+  containerElement.innerHTML = '';
+
+  for (const p of project) {
+    const article = document.createElement('article');
+
+    article.innerHTML = `
+        <${headingLevel}>${p.title}</${headingLevel}>
+        <img src="${p.image}" alt="${p.title}">
+        <p>${p.description}</p>
+    `;
+
+    containerElement.appendChild(article);
+  }
+
+}
---
+++ projects/projects.js
@@ -0,0 +1,11 @@
+import { fetchJSON, renderProjects } from '../global.js';
+
+const projects = await fetchJSON('../lib/projects.json');
+const projectsContainer = document.querySelector('.projects');
+const titleElement = document.querySelector('.projects-title');
+
+renderProjects(projects, projectsContainer, 'h2');
+
+if (titleElement && Array.isArray(projects)) {
+  titleElement.textContent = `${projects.length} Projects`;
+}
